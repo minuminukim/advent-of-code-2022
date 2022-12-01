193781b0d9ce751633568cc68ae8bd3d0fb51836
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-12-01 08:45:45 -0500

    Solve day 1

---
+++ day-1/index.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+
+function parseInput(filePath) {
+  return fs
+    .readFileSync(filePath, { encoding: "utf8" })
+    .split("\n\n")
+    .map((block) => block.split("\n").map(Number));
+}
+
+function a(lines) {
+  let bestSum = 0;
+  for (const line of lines) {
+    bestSum = Math.max(
+      bestSum,
+      line.reduce((sum, current) => sum + current),
+      0
+    );
+  }
+  return bestSum;
+}
+
+function solveA(lines) {
+  return lines.reduce(
+    (bestSum, line) =>
+      Math.max(
+        bestSum,
+        line.reduce((sum, current) => sum + current, 0)
+      ),
+    0
+  );
+}
+
+console.log(solveA(parseInput("./input.txt"))); // 69912
+
+function solveB(lines) {
+  const [first, second, third] = lines
+    .map((line) => line.reduce((sum, current) => sum + current, 0))
+    .sort((a, b) => b - a);
+  return first + second + third;
+}
+
+console.log(solveB(parseInput("./input.txt"))); // 208180
