ba421bb8aff1ac885571fdf7d0ef92219c82d33d
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-12-02 00:14:45 -0500

    solve d2p1

---
+++ day-2/index.js
@@ -0,0 +1,85 @@
+import fs from 'fs';
+
+function parseInput(filePath) {
+  return fs
+    .readFileSync(filePath, { encoding: 'utf-8' })
+    .trim()
+    .split('\n')
+    .map(pair => pair.split(' '));
+}
+
+const lines = parseInput('./input.txt');
+// console.log({ lines });
+const points = {
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const [left, right] = lines.reduce(
+  (sums, [l, r]) => {
+    sums[0] += points[l];
+    sums[1] += points[r];
+    return sums;
+  },
+  [0, 0]
+);
+
+// console.log({ left, right });
+
+const [winPointsLeft, winPointsRight] = lines.reduce(
+  (sums, [l, r]) => {
+    let [left, right] = sums;
+    switch (l) {
+      case 'A':
+        if (r === 'X') {
+          left += 3;
+          right += 3;
+        } else if (r === 'Y') {
+          left += 0;
+          right += 6;
+        } else if (r === 'Z') {
+          left += 6;
+          right += 0;
+        }
+        break;
+      case 'B':
+        if (r === 'X') {
+          left += 6;
+          right += 0;
+        } else if (r === 'Y') {
+          left += 3;
+          right += 3;
+        } else {
+          left += 0;
+          right += 6;
+        }
+        break;
+      case 'C':
+        if (r === 'X') {
+          left += 0;
+          right += 6;
+        } else if (r === 'Y') {
+          left += 6;
+          right += 0;
+        } else {
+          left += 3;
+          right += 3;
+        }
+        break;
+      default:
+        break;
+    }
+    return [left, right];
+  },
+  [0, 0]
+);
+
+// console.log({ winPointsLeft, winPointsRight });
+
+const leftTotal = left + winPointsLeft;
+const rightTotal = right + winPointsRight;
+// console.log({ rightTotal }); // 13682
